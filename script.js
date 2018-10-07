1e338bb3f5229d3e5961b73e9ba14f38790b7f31
Kamil Olber <olb.kamil@gmail.com>
2018-10-07 09:36:28 +0200

    zad 3

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+//// Zad 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/// zad 3
+/*/
+function forEach(arr,fn){
+    for (let i = 0; i < arr.length - 1; i++) {
+       // console.log(arr[i]);
+       fn(arr[1]);
+    }
+
+}
+const names = ['Janek', 'Wiola', 'Mateusz', 'Kamila', 'Olaf', 'Sylwia'];
+forEach(names, console.log);
+
+*/
+
+
+
+// zad 2
+/*
+const hello = function(){
+    
+    console.log('cześć');
+   }
+   
+   const helloCopy = hello;
+    hello();
+    helloCopy();
+    function call(fn){
+        fn();
+    }
+   
+    call(hello);
+    call(helloCopy);
+
+    */
+
 // zad 1
 /*
 function forEach(arr){
@@ -10,19 +65,3 @@ const names = ['Janek', 'Wiola', 'Mateusz', 'Kamila', 'Olaf', 'Sylwia'];
 forEach(names);
 
 */
-// zad 2
-
-const hello = function(){
-    
- console.log('cześć');
-}
-
-const helloCopy = hello;
- hello();
- helloCopy();
- function call(fn){
-     fn();
- }
-
- call(hello);
- call(helloCopy);
