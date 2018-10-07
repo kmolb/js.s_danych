05a5c09be0e41570a34d83b4ac3fbe8109cea02b
Kamil Olber <olb.kamil@gmail.com>
2018-10-07 11:08:59 +0200

    zad 7. Funkcje find

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
+/////// zad 7
+const people = [{
+        name: 'Janek',
+        age: 31
+    },
+    {
+        name: 'Wiola',
+        age: 35
+    },
+    {
+        name: 'Mateusz',
+        age: 29
+    },
+    {
+        name: 'Kamila',
+        age: 25
+    },
+    {
+        name: 'Olaf',
+        age: 33
+    },
+    {
+        name: 'Sylwia',
+        age: 25
+    }
+];
+
+people.find = function (fn) {
+    for (let i = 0; i <= this.length - 1; i++) {
+        if (fn(this[i])) {
+            return this[i];
+        }
+    }
+    return undefined;
+};
+people.find((arg) => { 
+    return arg.age === 25;
+   });
+   
+
+
 
 ////// zad 6
-
+/*
 function forEach(fn) {
     for (let i = 0; i < this.length - 1; i++) {
         // console.log(arr[i]);
@@ -15,8 +56,7 @@ names.forEach(function (arg)  {
       console.log(arg)
     });
 
-
-
+*/
 ///// zad 5
 /*
 function forEach(fn) {
