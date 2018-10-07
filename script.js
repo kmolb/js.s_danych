14fe46df976aa1aa4e1cecf1ee444825096ddd51
Kamil Olber <olb.kamil@gmail.com>
2018-10-07 08:50:50 +0200

    array + petla for/each

---
+++ script.js
@@ -0,0 +1,8 @@
+function forEach(arr){
+    for (let i = 0; i < arr.length - 1; i++) {
+        console.log(arr[i]);
+    }
+
+}
+const names = ['Janek', 'Wiola', 'Mateusz', 'Kamila', 'Olaf', 'Sylwia'];
+forEach(names);
