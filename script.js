735af27acdd200e1274e3698fabfc7a5ce241908
Kamil Olber <olb.kamil@gmail.com>
2018-10-07 10:03:00 +0200

    zad 5 this

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
+///// zad 5
+
+
+function forEach(fn){
+    for (let i = 0; i < this.length - 1; i++) {
+       // console.log(arr[i]);
+       fn(this[i]);
+    }
+
+}
+const names = ['Janek', 'Wiola', 'Mateusz', 'Kamila', 'Olaf', 'Sylwia'];
+
+
+
+
+names.forEach = forEach;
+names.forEach( console.log);
 //// Zad 4
 
-let persone = {
+/* 
+
+const persone = {
     name : "Igor",
     hello : function(){
-        console.log(this.name )
+        console.log(this.name )  // this wskazuje na obiekt ktory go wywołał
      }
 };
-
 persone.hello();
 
-
-
-
-
-
-
-
-
+**/
 /// zad 3
 /*/
 function forEach(arr,fn){
@@ -24,32 +35,25 @@ function forEach(arr,fn){
        // console.log(arr[i]);
        fn(arr[1]);
     }
-
 }
 const names = ['Janek', 'Wiola', 'Mateusz', 'Kamila', 'Olaf', 'Sylwia'];
 forEach(names, console.log);
 
 */
-
-
-
 // zad 2
 /*
 const hello = function(){
     
     console.log('cześć');
    }
-   
    const helloCopy = hello;
     hello();
     helloCopy();
     function call(fn){
         fn();
     }
-   
     call(hello);
     call(helloCopy);
-
     */
 
 // zad 1
@@ -58,7 +62,6 @@ function forEach(arr){
     for (let i = 0; i < arr.length - 1; i++) {
         console.log(arr[i]);
     }
-
 }
 const names = ['Janek', 'Wiola', 'Mateusz', 'Kamila', 'Olaf', 'Sylwia'];
 forEach(names);
