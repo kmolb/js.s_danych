ee06e997320f05179b5adab5cec47c29aa54c46d
Kamil Olber <olb.kamil@gmail.com>
2018-10-07 09:10:37 +0200

    funkcj wyzszego rzedu, podmiana funkcji

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// zad 1
+/*
 function forEach(arr){
     for (let i = 0; i < arr.length - 1; i++) {
         console.log(arr[i]);
@@ -6,3 +8,21 @@ function forEach(arr){
 }
 const names = ['Janek', 'Wiola', 'Mateusz', 'Kamila', 'Olaf', 'Sylwia'];
 forEach(names);
+
+*/
+// zad 2
+
+const hello = function(){
+    
+ console.log('cześć');
+}
+
+const helloCopy = hello;
+ hello();
+ helloCopy();
+ function call(fn){
+     fn();
+ }
+
+ call(hello);
+ call(helloCopy);
